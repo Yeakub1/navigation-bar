948c05c53cc21768f470fe59ea027a5174764d89
Yeakub1 <mryeakub10@gmail.com>
2023-12-25 23:02:53 +0600

    navigation bar

---
+++ script.js
@@ -0,0 +1,59 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+
+searchBox.addEventListener("click", () => {
+  navbar.classList.toggle("showInput");
+  if (navbar.classList.contains("showInput")) {
+    searchBox.classList.replace("bx-search", "bx-x");
+  } else {
+    searchBox.classList.replace("bx-x", "bx-search");
+  }
+});
+
+
+// toggle dark and light mode
+let modeToggle = document.querySelector(".dark-light");
+let body = document.body;
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode") {
+  body.classList.add("dark");
+}
+
+modeToggle.addEventListener("click", () => {
+  modeToggle.classList.toggle("active");
+  body.classList.toggle("dark");
+
+  if (!body.classList.contains("dark")) {
+    localStorage.setItem("mode", "light-mode");
+  } else {
+    localStorage.setItem("mode", "dark-mode");
+  }
+});
+
+      
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+};
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+};
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+};
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function () {
+  navLinks.classList.toggle("show2");
+};
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+  navLinks.classList.toggle("show3");
+};
